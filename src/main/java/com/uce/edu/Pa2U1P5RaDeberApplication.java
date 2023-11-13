fbb017559f27342829f0bc07d60982eb616fe654
rdaguas <aguas.rdv2001@gmail.com>
2023-11-12 22:20:40 -0500

    Primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5RaDeberApplication.java
@@ -0,0 +1,21 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5RaDeberApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5RaDeberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Roberto Aguas: Proyecto Deberes");
+		
+	}
+
+}
